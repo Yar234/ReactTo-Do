16e51be98c9773e3d8aa90fdb7c94160d0af1440
Yar <yaroslavvnukov66@gmail.com>
2025-06-04 21:59:45 -0700

    start to create the check function

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -7,6 +7,15 @@ export default function TaskList({ tasks = [], setTasks }) {
     setTasks((prev) => prev.filter((task) => task.id !== id));
   }
 
+  // some tasks may have a completed state, but it's not used in this component
+  function handleCheck(id) {
+    setTasks((prev) =>
+      prev.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  }
+
   return (
     <div className={`${styles.task_list} ${styles.center}`}>
       {tasks.map((task) => (
